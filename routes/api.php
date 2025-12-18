ca6cb334c41c00e7d961756b82cee6bdc3f4aef4
Nasrin Mokhtari <nasrin.mokhtari319@gmail.com>
2025-12-18 18:41:11 +0330

    first commit

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('user', function (Request $request) {
+    $user = User::where('mobile', '=', $request['mobile'])->first();
+
+    if ($user === null) {
+        $user = User::create(['mobile' => $request['mobile'], 'name' => $request['name'],]);
+    }
+
+    return response()->json(['data' => $user]);
+});
+
+
+Route::put('user', function (Request $request) {
+    $user = User::where('mobile', '=', $request['mobile'])->first();
+
+    if ($user !== null) {
+        $user = $user->fill(['variant' => $request['variant'], 'gender' => $request['gender']])->save();
+    }
+
+    return response()->json(['data' => $user]);
+});
